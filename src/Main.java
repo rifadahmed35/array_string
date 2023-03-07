d8c7c452388fbea8574336052b3ee44ffaf1bddb
Md Rifad Ahmed <rifad.mbstu@gmail.com>
2023-03-07 23:18:09 +0100

    character array

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        char arr[] = {'H', 'e', 'l', 'l', 'o', 'w'};
+        {
+            System.out.println(arr);
+            //string is nothing - but a character array.
+        }
+        {
+            System.out.println(arr[5]);
+        }
+        {
+            System.out.println("The length of array:" +arr.length);
+        }
+    }
+}
